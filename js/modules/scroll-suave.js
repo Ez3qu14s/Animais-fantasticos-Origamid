acaa005a3ce6bb678cb87658eb65aec4b646af99
Ezequias Oliveira <ezequias.oliveira@catskillet.com>
2023-07-04 15:35:48 -0300

    feat: refatoracao ScrollSuave em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,27 +1,37 @@
-export default function initSmoothScroll() {
-  const linksInternos = document.querySelectorAll(
-    '[data-menu="suave"] a[href^="#"]',
-  );
+export default class smoothScroll {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
 
-  function scrollToSection(event) {
+    if (options === undefined) {
+      this.options = {
+        behavior: 'smooth',
+        block: 'start',
+      };
+    } else {
+      this.options = options;
+    }
+
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
-    // const topo = section.offsetTop;
 
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-
-    // FORMA ALTERNATIVA
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: "smooth",
-    // });
+    section.scrollIntoView(this.options);
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  });
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
