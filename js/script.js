b64f32a214edc69a22832a3b174114f64c032134
Ezequias Oliveira <ezequias.oliveira@catskillet.com>
2023-07-05 15:08:42 -0300

    feat: modal refatorada com class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-import ScrollSuave from './modules/scroll-suave.js';
-import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdown from './modules/dropdown.js';
 import initMenuMobile from './modules/menu-mobile.js';
@@ -7,8 +5,10 @@ import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
+import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
+import Modal from './modules/modal.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -19,8 +19,10 @@ accordion.init();
 const tabNav = new TabNav("[data-tab='menu'] li", "[data-tab='content'] section");
 tabNav.init();
 
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
+
 initAnimacaoScroll();
-initModal();
 initTooltip();
 initDropdown();
 initMenuMobile();
