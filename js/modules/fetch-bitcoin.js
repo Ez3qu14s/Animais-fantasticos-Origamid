7118ed86fb31d37e9f6669e9d0bd7849f77de5a2
Ezequias Oliveira <ezequias.oliveira@catskillet.com>
2023-07-07 14:50:14 -0300

    usuário 2 adicionou console

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,13 +1,15 @@
-export default function initFetchBitcoin() {}
-
+export default function initFetchBitcoin() {
+  console.log('teste')
 // https://blockchain.info/ticker
 
-fetch('https://blockchain.info/ticker')
-  .then((response) => response.json())
-  .then((json) => {
-    const btcPreco = document.querySelector('.btc-preco');
-    btcPreco.innerText = (1000 / json.BRL.sell).toFixed(4);
-  })
-  .catch((erro) => {
-    console.log(Error(erro));
-  });
+  fetch('https://blockchain.info/ticker')
+    .then((response) => response.json())
+    .then((json) => {
+      const btcPreco = document.querySelector('.btc-preco');
+      btcPreco.innerText = (1000 / json.BRL.sell).toFixed(4);
+    })
+    .catch((erro) => {
+      console.log(Error(erro));
+    });
+}
+  
